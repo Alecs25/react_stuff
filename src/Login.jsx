1f093a0e1dd8a6008972ac5bd63b19d6dca20785
Alex <alex00romagnoli@gmail.com>
2024-02-09 23:59:40 +0100

    first commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Login() {
 	const [userName, setUserName] = useState("");
 	const [password, setPassword] = useState("");
+	const [buttonEnabler, setButtonEnabler] = useState(false);
+    const[loggedIn, setLoggedIn] = useState(false)
 
+
+	useEffect(() => {
+		if (userName !== "" && password !== "") {
+			setButtonEnabler(false);
+			console.log(buttonEnabler);
+		} else {
+			setButtonEnabler(true);
+			console.log(userName, password);
+		}
+	}, [userName, password]);
+
+    function onLogin(){
+        setLoggedIn(true)
+        alert("loggato")
+    }
+	
 	function handleChange(e) {
-		console.log(e);
+		//console.log(e);
 
 		if (e.target.name === "user") {
 			setUserName(e.target.value);
@@ -19,6 +37,9 @@ export function Login() {
 			<input name="user" type="text" onChange={handleChange} value={userName} />
 			<input name="pass" type="text" onChange={handleChange} value={password} />
 			<input type="checkbox" />
+			<button onClick={onLogin} disabled={buttonEnabler}>
+				Login
+			</button>
 		</div>
 	);
 }
