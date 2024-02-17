fbea24de2ed6ee3a6a1d7b00908fcb6851c32256
Alex <alex00romagnoli@gmail.com>
2024-02-17 10:40:42 +0100

    first commit

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -2,36 +2,15 @@
 // The component should render the user's name, login and avatar.
 
 import { useEffect, useState } from "react";
+import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
-	const [userData, setUserData] = useState(null);
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(null);
-	useEffect(() => {
-		async function fetchGithubUser(username) {
-			setLoading(true);
-			setUserData(null);
-			try {
-				const response = await fetch(`https://api.github.com/users/${username}`);
-				if (response.status !== "200") {
-					throw new Error("user not found");
-				} else {
-					const json = await response.json();
-					setUserData(json);
-				}
-			} catch (error) {
-				setError(error.message);
-			} finally {
-				setLoading(false);
-			}
-		}
-		fetchGithubUser(username);
-	}, []);
+	const { userData, error, isLoading } = useGithubUser(username );
 
 	return (
 		<div>
 			{error && <p>{error}</p>}
-			{loading && <p>Loading data...</p>}
+			{isLoading && <p>Loading data...</p>}
 			{userData && <p>User:{userData.login}</p>}
 			{userData && <p>Name:{userData.name}</p>}
 			{userData && (
