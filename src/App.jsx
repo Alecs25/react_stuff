0ba3c369455dc1850567afac4883c4589bef5a2d
Alex <alex00romagnoli@gmail.com>
2024-02-20 21:09:05 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Welcome } from "./Welcome";
 
 function App() {
-
-
-  return (
-    
-   
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/Welcome" element={<Welcome name={"User"} />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
+
+// Create a new branch starting from the first props exercise.
+// Modify App component that wraps a Routes component and add a single Route to the /
+// path that renders the Welcome component, passing it a name prop. Render the App component within a BrowserRouter component.
