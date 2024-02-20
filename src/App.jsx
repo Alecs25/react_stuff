c770764ca36f4da273d4f85cf5429158dd0c7c05
Alex <alex00romagnoli@gmail.com>
2024-02-20 21:31:08 +0100

    base per routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Welcome } from "./Welcome";
-import {GithubUser } from "./GithubUser";
-
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/Welcome" element={<Welcome name={"User"} />}></Route>
-				<Route path="/GithubUser" element={<GithubUser username={"Alecs25"} />}></Route>
+				<Route></Route>
 			</Routes>
 		</BrowserRouter>
 	);
 }
 
 export default App;
-
-// Create a new branch starting from the first props exercise.
-// Modify App component that wraps a Routes component and add a single Route to the /
-// path that renders the Welcome component, passing it a name prop. Render the App component within a BrowserRouter component.
